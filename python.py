3627f0f874961093f2e751a31efe09f02fe66dd8
Tanika180 <113040533+Tanika180@users.noreply.github.com>
2022-09-14 20:09:05 +0530

    Update python.py

--- python.py
+++ python.py
@@ -7,4 +7,8 @@ def most_frequent(string):
             d[key] += 1
     return d
 A=most_frequent('Mississippi')
+def sort_dict_by_value(d, reverse = False):
+  return dict(sorted(d.items(), key = lambda x: x[1], reverse = reverse))
 print(A)
+print("\nSort (descending) the said dictionary elements by value:")
+print(sort_dict_by_value(A, True))
